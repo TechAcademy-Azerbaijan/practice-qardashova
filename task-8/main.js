c6908d0f66f174ddc940e03d37f43c0d96df8165
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-05 10:52:39 +0300

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -5,5 +5,19 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+   let n = result.input; 
+  let i = n.split(',')[0]
+  let b = n.split(',')[1]
+  let hasil = 1;
+  for (  i  ; i <= b; i++) {
+    hasil = hasil * i;
+  }
+
+  if (hasil > 0) {
+    console.log("1");
+  }
+  else{
+    console.log("0");
+  }
   
 });
