5c455eacfc1bef235dc3e8132df34c7ecfbfa9bb
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-12 10:47:51 +0300

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -5,5 +5,22 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let n = result.input;
+  let arr = n.split(' ');
+  let res = true;
+  let count = 0;
+  
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      count++
+    }
+      
+  }
+  if (count == 0) {
+    console.log("Yes");
+  }
+  else{
+    console.log("No");
+  }
   
 });
