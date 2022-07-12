adda0fa81eaad0400c312f7b2e2a80deb7c8d4f5
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-12 09:28:46 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -5,5 +5,14 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+    let n = parseInt(result.input);
+  let fibo = 0;
+  if (n == 1 || n==0) {
+    console.log(1);
+  }
+  else{
+    fibo = fibo + n-1 + n-2;
+    console.log(fibo);
+  }
   
 });
