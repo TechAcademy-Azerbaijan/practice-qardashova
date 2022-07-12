70c346a0287fa413914c4b42c686cd05becbf603
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-12 10:49:20 +0300

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -5,5 +5,11 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+    let n = parseInt(result.input);  
+  for (let i = 1; i < n; i++) {
+    if (i*i < n) {
+      console.log(i*i);
+    }
+  }
   
 });
