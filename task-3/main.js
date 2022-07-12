a339923a8d62cca7b4f7ef3da2a1c5d84badb97c
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-12 09:25:50 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -5,5 +5,11 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+    let n = result.input;
+  let arr = n.split(',');
+  for (let i = (arr.length - 1); i >= 0; i--) {
+    console.log(arr[i]);  
+  }
+  console.log(arr);
   
 });
