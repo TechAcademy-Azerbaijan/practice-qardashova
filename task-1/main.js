07dd33f9e78d106bf1294aa06e16cfc91be69c46
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-12 09:19:54 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -5,5 +5,7 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let n = parseInt(result.input);
+  console.log(parseInt(n/100)*100 + parseInt((n%100)/20)*30 +((n%100)%20)*2);
   
 });
