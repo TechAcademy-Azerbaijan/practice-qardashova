9d6c28443f569117d3f147a003b2f96aa83eb192
qardashova <58748372+qardashova@users.noreply.github.com>
2022-07-12 09:55:52 +0300

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -5,5 +5,7 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
-  
+    let n = result.input;
+  let arr = n.split(',');
+  console.log(Math.max(...arr));
 });
